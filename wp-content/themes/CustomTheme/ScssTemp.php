8755d247e00b3f07b3430a3143d607ad24fa2f66
Adarsh Sharma <adarsh.sharma@quovantis.com>
2021-01-29 18:41:54 +0500

    add scss work name called scss temp

---
+++ wp-content/themes/CustomTheme/ScssTemp.php
@@ -0,0 +1,17 @@
+<?php /* Template Name: Sccs Temp */ ?>
+<?php get_header(); ?>
+<div class="container">
+    <div class="main">
+        <p class="main_paragraph">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Ducimus, assumenda molestias sit similique cumque, aperiam voluptate, quasi delectus explicabo accusamus accusantium beatae.
+            Natus tenetur repudiandae aliquid minus dolorum ad veritatis.
+        </p>
+        <p class="main_paragraph">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Ducimus, assumenda molestias sit similique cumque, aperiam voluptate, quasi delectus explicabo accusamus accusantium beatae.
+            Natus tenetur repudiandae aliquid minus dolorum ad veritatis.
+        </p>
+    </div>
+</div>
+<?php get_footer(); ?>
